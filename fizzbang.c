edd237feb42ce779a133b97eca12e38221f121ff
Surya Das <surya.das@hotmail.com>
2013-08-28 21:50:55 +1000

    Solution inC

---
+++ fizzbang.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main(){
+	int i = 0;
+	while(int i < 100){
+		if i % 3 == 0{
+			print("Fizz");
+		}
+	    else if i % 5 == 0{
+	    	print("Buzz");
+	    }
+	    else{
+	    	print(%d, i);
+	    }
+	    i++;
+	}
+}
